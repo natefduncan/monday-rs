db091cde365961a1db96158d1085fdab5e2a1ea0
Nathan Duncan <natefduncan@gmail.com>
2021-07-26 18:53:23 -0500

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use graphql_client::{GraphQLQuery, Response};
-mod monday; 
+mod monday;
 
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "schema.json",
     query_path = "queries/board.graphql",
-    response_derives = "Debug,Serialize",
+    response_derives = "Debug,Serialize"
 )]
 struct BoardView;
 
 fn main() {
     let client = monday::get_client().expect("Could not get client.");
     let variables = board_view::Variables {
-        board_id : Some(vec![Some(1393475156)])
-    }; 
-    let res : Response<board_view::ResponseData> = monday::query::<BoardView>(&client, variables).expect("Could not execute query."); 
-    println!("{:#?}", res); 
+        board_id: Some(vec![Some(1393475156)]),
+    };
+    let res: Response<board_view::ResponseData> =
+        monday::query::<BoardView>(&client, variables).expect("Could not execute query.");
+    println!("{:#?}", res);
 }
--- src/monday.rs
+++ src/monday.rs
@@ -1,25 +1,29 @@
 use graphql_client::reqwest::post_graphql_blocking;
-use reqwest::blocking::Client; 
 use graphql_client::{GraphQLQuery, Response};
+use reqwest::blocking::Client;
 
-const MONDAY_URL : &str = "https://api.monday.com/v2";  
+const MONDAY_URL: &str = "https://api.monday.com/v2";
 
 pub fn get_client() -> Result<Client, reqwest::Error> {
-    let monday_api_token = std::env::var("MONDAY_API_TOKEN").expect("Missing MONDAY_API_TOKEN env var");
+    let monday_api_token =
+        std::env::var("MONDAY_API_TOKEN").expect("Missing MONDAY_API_TOKEN env var");
     Client::builder()
-    .user_agent("monday-rust/0.1.0")
-    .default_headers(
-        std::iter::once((
-            reqwest::header::AUTHORIZATION,
-            reqwest::header::HeaderValue::from_str(&format!("{}", monday_api_token))
-                .unwrap(),
-        ))
-        .collect(),
-    )
-    .build()
+        .user_agent("monday-rust/0.1.0")
+        .default_headers(
+            std::iter::once((
+                reqwest::header::AUTHORIZATION,
+                reqwest::header::HeaderValue::from_str(&format!("{}", monday_api_token)).unwrap(),
+            ))
+            .collect(),
+        )
+        .build()
 }
 
-pub fn query<Q: GraphQLQuery>(client : &Client, variables : Q::Variables) -> Result<Response<Q::ResponseData>, reqwest::Error> {
-    let res : Response<Q::ResponseData> = post_graphql_blocking::<Q, _>(&client, MONDAY_URL, variables).expect("Request failed.");
+pub fn query<Q: GraphQLQuery>(
+    client: &Client,
+    variables: Q::Variables,
+) -> Result<Response<Q::ResponseData>, reqwest::Error> {
+    let res: Response<Q::ResponseData> =
+        post_graphql_blocking::<Q, _>(&client, MONDAY_URL, variables).expect("Request failed.");
     Ok(res)
-} 
+}
