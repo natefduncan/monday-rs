eb8987c11377a85d3aaec8527bc361376d006665
natefduncan <natefduncan@gmail.coM>
2021-07-27 22:23:04 -0700

    working tui + crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,272 @@
 use std::io;
-use tui::Terminal;
-use tui::backend::CrosstermBackend;
-use tui::widgets::{Block, Borders, ListItem, List};
-use tui::layout::{Layout, Constraint, Direction};
-use tui::style::{Color, Style, Modifier}; 
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{
+        Block, BorderType, Borders, Cell, List, ListItem, ListState, Paragraph, Row, Table, Tabs,
+    },
+    Terminal,
+};
+
+use crossterm::{
+    event::{self, EnableMouseCapture, Event as CEvent, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen},
+};
+
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration, Instant};
+use reqwest::blocking::Client;
 
 mod monday; 
 mod objects; 
 mod queries; 
 
-fn main() -> Result<(), io::Error> {
+//Event loop enum
+enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+//Menu enum
+#[derive(Copy, Clone, Debug)]
+enum MenuItem {
+    Home, 
+    Boards,
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Boards => 1,
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = monday::get_client().expect("Could not get client.");
-    let stdout = io::stdout();
+
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    terminal.draw(|f| {
-        //Set Chunks
-        let chunks = Layout::default()
-            .direction(Direction::Horizontal)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(20),
-                    Constraint::Percentage(80),
-                ].as_ref()
-            )
-            .split(f.size());
-        //Create search block
-        let block = Block::default()
-             .title("Boards")
-             .borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
-        //Create list of boards.
-        let boards : Vec<objects::Board> = queries::board_list(&client); 
-        let board_items : Vec<ListItem> = boards.iter().map(|x| ListItem::new(x.name.to_owned())).collect(); 
-        List::new(board_items)
-            .block(Block::default()
-                .title("Detail")
-                .borders(Borders::ALL)
-            )
-            .style(Style::default().fg(Color::White))
-            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-            .highlight_symbol(">>");
+
+    // Setup input handling
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            // poll for tick rate duration, if no events, sent tick event.
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+            if event::poll(timeout).unwrap() {
+                if let CEvent::Key(key) = event::read().unwrap() {
+                    tx.send(Event::Input(key)).unwrap();
+                }
+            }
+            if last_tick.elapsed() >= tick_rate {
+                tx.send(Event::Tick).unwrap();
+                last_tick = Instant::now();
+            }
+        }
+    });
+
+    let menu_titles = vec!["Home", "Boards", "Quit"];
+    let mut active_menu_item = MenuItem::Boards;
+    let mut board_list_state = ListState::default();
+    board_list_state.select(Some(0));
+
+    terminal.clear()?; 
+
+    loop {
+        terminal.draw(|rect| {
+            let size = rect.size();
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Min(2),
+                        Constraint::Length(3),
+                    ]
+                    .as_ref(),
+                )
+                .split(size);
+
+            let copyright = Paragraph::new("Monday-CLI 2021 - all rights reserved")
+                .style(Style::default().fg(Color::LightCyan))
+                .alignment(Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::White))
+                        .title("Copyright")
+                        .border_type(BorderType::Plain),
+                );
+
+            let menu = menu_titles
+                .iter()
+                .map(|t| {
+                    let (first, rest) = t.split_at(1);
+                    Spans::from(vec![
+                        Span::styled(
+                            first,
+                            Style::default()
+                                .fg(Color::Yellow)
+                                .add_modifier(Modifier::UNDERLINED),
+                        ),
+                        Span::styled(rest, Style::default().fg(Color::White)),
+                    ])
+                })
+                .collect();
+
+            let tabs = Tabs::new(menu)
+                .select(active_menu_item.into())
+                .block(Block::default().title("Menu").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White))
+                .highlight_style(Style::default().fg(Color::Yellow))
+                .divider(Span::raw("|"));
+
+            rect.render_widget(tabs, chunks[0]);
+            match active_menu_item {
+                MenuItem::Home => rect.render_widget(render_home(), chunks[1]),
+                MenuItem::Boards => {
+                    let board_chunks = Layout::default()
+                        .direction(Direction::Horizontal)
+                        .constraints(
+                            [Constraint::Percentage(20), Constraint::Percentage(80)].as_ref(),
+                        )
+                        .split(chunks[1]);
+                    let (left, right) = render_boards(&client, &board_list_state);
+                    rect.render_stateful_widget(left, board_chunks[0], &mut board_list_state);
+                    rect.render_widget(right, board_chunks[1]);
+                }
+            }
+            rect.render_widget(copyright, chunks[2]);
         })?;
+
+        match rx.recv()? {
+            Event::Input(event) => match event.code {
+                KeyCode::Char('q') => {
+                    disable_raw_mode()?;
+                    terminal.show_cursor()?;
+                    break;
+                }
+                KeyCode::Char('h') => active_menu_item = MenuItem::Home,
+                KeyCode::Char('b') => active_menu_item = MenuItem::Boards,
+                KeyCode::Down => {
+                    if let Some(selected) = board_list_state.selected() {
+                        let amount_boards = 10; 
+                        if selected >= amount_boards - 1 {
+                            board_list_state.select(Some(0));
+                        } else {
+                            board_list_state.select(Some(selected + 1));
+                        }
+                    }
+                }
+                KeyCode::Up => {
+                    if let Some(selected) = board_list_state.selected() {
+                        let amount_boards = 10; 
+                        if selected > 0 {
+                            board_list_state.select(Some(selected - 1));
+                        } else {
+                            board_list_state.select(Some(amount_boards - 1));
+                        }
+                    }
+                }
+                _ => {}
+            },
+            Event::Tick => {}
+        }
+    }
+
     Ok(())
 }
 
+fn render_home<'a>() -> Paragraph<'a> {
+    let home = Paragraph::new(vec![
+        Spans::from(vec![Span::raw("")]),
+        Spans::from(vec![Span::raw("Welcome")]),
+        Spans::from(vec![Span::raw("")]),
+        Spans::from(vec![Span::raw("to")]),
+        Spans::from(vec![Span::raw("")]),
+        Spans::from(vec![Span::styled(
+            "Monday-CLI",
+            Style::default().fg(Color::LightBlue),
+        )]),
+        Spans::from(vec![Span::raw("")]),
+        Spans::from(vec![Span::raw("Press 'b' to access boards")]),
+    ])
+    .alignment(Alignment::Center)
+    .block(
+        Block::default()
+            .borders(Borders::ALL)
+            .style(Style::default().fg(Color::White))
+            .title("Home")
+            .border_type(BorderType::Plain),
+    );
+    home
+}
+
+fn render_boards<'a>(client : &Client, board_list_state : &ListState) -> (List<'a>, Table<'a>) {
+    let board_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().fg(Color::White))
+        .title("Boards")
+        .border_type(BorderType::Plain);
+
+    let board_vec : Vec<objects::Board> = queries::board_list(&client); 
+    let list_items : Vec<ListItem> = board_vec.iter().map(|x| ListItem::new(x.name.to_owned())).collect();
+    let selected_board = board_vec
+        .get(
+            board_list_state
+                .selected()
+                .expect("there is always a selected board"),
+        )
+        .expect("exists")
+        .clone();
+
+    let board_list = List::new(list_items).block(board_block).highlight_style(
+        Style::default()
+            .bg(Color::Yellow)
+            .fg(Color::Black)
+            .add_modifier(Modifier::BOLD),
+    );
+
+    let board_detail =  Table::new(vec![Row::new(vec![
+        Cell::from(Span::raw(selected_board.id.to_string())),
+        Cell::from(Span::raw(selected_board.name)),
+    ])])
+    .header(Row::new(vec![
+        Cell::from(Span::styled(
+            "ID",
+            Style::default().add_modifier(Modifier::BOLD),
+        )),
+        Cell::from(Span::styled(
+            "Name",
+            Style::default().add_modifier(Modifier::BOLD),
+        )),
+    ]))
+    .block(
+        Block::default()
+            .borders(Borders::ALL)
+            .style(Style::default().fg(Color::White))
+            .title("Detail")
+            .border_type(BorderType::Plain),
+    )
+    .widths(&[
+        Constraint::Percentage(50),
+        Constraint::Percentage(50),
+    ]);
+
+    (board_list, board_detail)
+}
 
