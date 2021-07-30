ddf3b8ad50f2e4a2d8c4492dc4802a83f332c811
natefduncan <natefduncan@gmail.coM>
2021-07-30 15:43:19 -0700

    events file for event handling

---
+++ src/events.rs
@@ -0,0 +1,35 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration, Instant};
+use crossterm::event::{self, Event as CEvent, KeyEvent}; 
+
+//Event loop enum
+pub enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+pub fn start_input_handling() -> mpsc::Receiver<Event<KeyEvent>> {
+    // Setup input handling
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            // poll for tick rate duration, if no events, sent tick event.
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+            if event::poll(timeout).unwrap() {
+                if let CEvent::Key(key) = event::read().unwrap() {
+                    tx.send(Event::Input(key)).unwrap();
+                }
+            }
+            if last_tick.elapsed() >= tick_rate {
+                tx.send(Event::Tick).unwrap();
+                last_tick = Instant::now();
+            }
+        }
+    });
+    return rx; 
+}
