89c4041dc2613c11b850b3388554151a16c6047d
natefduncan <natefduncan@gmail.coM>
2021-08-01 23:12:34 -0500

    initial item detail

--- src/queries.rs
+++ src/queries.rs
@@ -41,28 +41,28 @@ fn parse_board_list_response(res: Response<board_list::ResponseData>) -> Vec<Boa
     return boards;
 }
 
-//BOARD DETAIL
+//ITEM LIST
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "schema.json",
-    query_path = "queries/board_items.graphql",
+    query_path = "queries/item_list.graphql",
     response_derives = "Debug,Clone"
 )]
-struct BoardDetail;
+struct ItemList;
 
-pub fn board_detail(client: &Client, board_id: String) -> Vec<Item> {
-    let variables = board_detail::Variables {
+pub fn item_list(client: &Client, board_id: String) -> Vec<Item> {
+    let variables = item_list::Variables {
         board_id: Some(board_id.parse::<i64>().expect("can convert to i64")),
         limit: Some(50),
         newest_first: Some(true),
         page: Some(1),
     };
-    let res: Response<board_detail::ResponseData> =
-        monday::query::<BoardDetail>(&client, variables).expect("Could not execute query.");
+    let res: Response<item_list::ResponseData> =
+        monday::query::<ItemList>(&client, variables).expect("Could not execute query.");
     parse_board_detail_response(res)
 }
 
-fn parse_board_detail_response(res: Response<board_detail::ResponseData>) -> Vec<Item> {
+fn parse_board_detail_response(res: Response<item_list::ResponseData>) -> Vec<Item> {
     let data = res.data.expect("missing response data.");
     let board = data
         .boards
@@ -78,13 +78,48 @@ fn parse_board_detail_response(res: Response<board_detail::ResponseData>) -> Vec
         .iter()
         .map(|item| {
             let i = item.clone().unwrap();
-            Item {
-                name: i.name,
-                group: Group {
-                    title: "".to_string(),
-                },
-            }
+            let mut item = Item::new();
+            item.id = i.id.parse::<u32>().unwrap().clone(); 
+            item.name = i.name.clone(); 
+            item
         })
         .collect::<Vec<Item>>();
     return items;
+}
+
+//ITEM DETAIL
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schema.json",
+    query_path = "queries/item_detail.graphql",
+    response_derives = "Debug,Clone"
+)]
+struct ItemDetail;
+
+type Date = String; 
+
+pub fn item_detail(client: &Client, item_id: u32) -> Item {
+    let variables = item_detail::Variables {
+        item_id: Some(item_id as i64),
+    };
+    let res: Response<item_detail::ResponseData> =
+        monday::query::<ItemDetail>(&client, variables).expect("Could not execute query.");
+    parse_item_detail_response(res)
+}
+
+fn parse_item_detail_response(res: Response<item_detail::ResponseData>) -> Item {
+    let data = res.data.expect("missing response data.");
+    let item = data
+        .items
+        .unwrap()
+        .into_iter()
+        .nth(0)
+        .expect("missing first value")
+        .unwrap();
+    let mut output = Item::new();
+    output.name = item.name;
+    output.id = item.id.parse::<u32>().unwrap(); 
+    output.updated_at = item.updated_at.unwrap(); 
+    return output; 
+
 }
