c794d294b75529653a817f42bda21585086a6daa
natefduncan <natefduncan@gmail.coM>
2021-08-01 21:31:11 -0500

    update search to sort by matches

--- src/utils.rs
+++ src/utils.rs
@@ -1,35 +1,37 @@
 use super::objects;
 use aho_corasick::AhoCorasickBuilder;
+use std::fmt;
 
-fn search(query : String, vector : &Vec<String>) -> Vec<bool> {
+fn search(query : String, vector : &Vec<String>) -> Vec<usize> {
     let patterns = query.split_whitespace(); 
     let ac = AhoCorasickBuilder::new()
         .ascii_case_insensitive(true)
         .build(patterns);
     vector.iter().map(|string| {
-        match ac.find(string).map(|m| m.pattern()) {
-            Some(v) => true, 
-            None => false
-        }
-    }).collect::<Vec<bool>>()
+        let matches = ac.find_iter(string).map(|m| m.pattern()).collect::<Vec<usize>>(); 
+        matches.len()
+    }).collect::<Vec<usize>>()
 }
 
-fn filter_by_bool<T : Clone>(filter_vec : &Vec<T>, bool_vec : &Vec<bool>) -> Vec<T> {
+#[derive(Debug, Copy, Clone)]
+struct Match<T> {
+    obj : T, 
+    count : usize
+}
+
+fn filter_by_matches<T : Clone + std::fmt::Debug >(filter_vec : &Vec<T>, match_vec : &Vec<usize>) -> Vec<T> {
     let mut output : Vec<T> = Vec::new(); 
-    let zip = filter_vec.iter().zip(bool_vec.iter()); 
-    for (i, val) in filter_vec.iter().enumerate() {
-        if bool_vec[i] {
-            output.push(val.clone()); 
-        }
-    }
-    return output; 
+    let zip = filter_vec.iter().zip(match_vec.iter()); 
+    let mut matches : Vec<Match<T>> = zip.map(|(x,m)| Match { obj : x.clone(), count : *m}).collect::<Vec<Match<T>>>();
+    matches.sort_by(|a, b| b.count.cmp(&a.count)); 
+    return matches.iter().filter(|m| m.count > 0).map(|m| m.obj.clone()).collect::<Vec<T>>(); 
 }
 
 pub fn search_boards(query: String, boards: &Vec<objects::Board>) -> Vec<objects::Board> {
     let query_lower: String = query.to_lowercase();
     let vec_string : Vec<String> = boards.iter().map(|board| board.name.clone()).collect::<Vec<String>>(); 
     let search_bool = search(query, &vec_string); 
-    let filtered = filter_by_bool::<objects::Board>(&boards, &search_bool); 
+    let filtered = filter_by_matches::<objects::Board>(&boards, &search_bool); 
     return filtered
 }
 
@@ -37,7 +39,7 @@ pub fn search_items(query: String, items: &Vec<objects::Item>) -> Vec<objects::I
     let query_lower: String = query.to_lowercase();
     let vec_string : Vec<String> = items.iter().map(|item| item.name.clone()).collect::<Vec<String>>(); 
     let search_bool = search(query, &vec_string); 
-    let filtered = filter_by_bool::<objects::Item>(&items, &search_bool); 
+    let filtered = filter_by_matches::<objects::Item>(&items, &search_bool); 
     return filtered
 } 
 
